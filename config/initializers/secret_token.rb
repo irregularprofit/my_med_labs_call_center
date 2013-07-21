fbbc72de554724a01349a6ea203576a073c78b2d
Tammam Kbeili <tammam.kbeili@gmail.com>
2013-07-21 11:29:29 -0700

    empty Rails 4 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyMedLabsCallCenter::Application.config.secret_key_base = 'e8afc77f20b2667f9f806a632d54c185d3d850bf00d4160fb75270294b5f17fcb29fb976f0100abb2ecbf55382189af37dcd5170ad94d2c01e3636c232be5da9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyMedLabsCallCenter::Application.config.session_store :cookie_store, key: '_my_med_labs_call_center_session'

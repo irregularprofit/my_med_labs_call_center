a99178fe1df8660576280b6420e2fb0880243a63
Jimmy Hsu <irregular.profit@gmail.com>
2013-07-22 22:38:07 -0700

    adding enqueue actins with background music

--- app/controllers/connects_controller.rb
+++ app/controllers/connects_controller.rb
@@ -4,6 +4,8 @@ class ConnectsController < ApplicationController
   before_filter :check_logged_in_user, only: :index
   skip_before_filter :verify_authenticity_token
 
+  CALLER_ID = "+14086457436"
+
   def index
     @client_name = params[:client]
     @client_name = current_user.name if @client_name.nil?
@@ -15,17 +17,16 @@ class ConnectsController < ApplicationController
     # Create an application sid at twilio.com/user/account/apps and use it here
     capability.allow_client_outgoing "APeabd53438dea1a6b79f651bd14c9c875"
     capability.allow_client_incoming @client_name
-    @token = capability.generate
-    render :index, locals: {token: @token, client_name: @client_name}
+    token = capability.generate
+    render :index, locals: {token: token, client_name: @client_name}
   end
 
   def voice
-    caller_id = "+14086457436"
     number = params[:PhoneNumber]
 
     response = Twilio::TwiML::Response.new do |r|
       # Should be your Twilio Number or a verified Caller ID
-      r.Dial callerId: caller_id do |d|
+      r.Dial callerId: CALLER_ID do |d|
         # Test to see if the PhoneNumber is a number, or a Client ID. In
         # this case, we detect a Client ID by the presence of non-numbers
         # in the PhoneNumber parameter.
@@ -38,6 +39,65 @@ class ConnectsController < ApplicationController
           d.Client number
         end
       end
+    end
+
+    render text: response.text
+  end
+
+  def enqueue
+    response = Twilio::TwiML::Response.new do |r|
+      r.Enqueue action: '/voice', waitUrl: '/wait_url.xml', waitUrlMethod: 'GET' do |d|
+
+      end
+    end
+    #FIXME: this is stupid but I can't figure out how to store nouns
+    response_text = response.text
+    response_text = response_text.gsub("</Enqueue>", "support</Enqueue>")
+
+    render text: response_text
+  end
+
+  def about_to_connect
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say "Thank you for waiting. An agent is about to connect with you now."
+    end
+
+  end
+
+  def queue
+    # response = Twilio::TwiML::Response.new do |r|
+    #   r.Dial do |d|
+    #     d.Queue action: '/about_to_connect.xml', method: 'GET'
+    #   end
+    # end
+
+    # response_text = response.text
+    # response_text = response_text.gsub("</Queue>", "support</Queue>")
+
+    # render text: response_text
+
+    account_sid = 'ACc9f94230884add84b3a5fa0d7c6df08a'
+    auth_token = '827e6c207363a1b7b8f1c5861ecc8fe9'
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    
+    @client.account.queues.list.each do |queue|
+      puts queue.send(:sid).inspect
+      puts queue.sid.inspect
+    end
+
+    # Get an object from its sid. If you do not have a sid,
+    # check out the list resource examples on this page
+    @member = @client.account.queues.get(sid).members.get("Front")
+    @member.update(url: "/voice.xml",
+        method: "POST")
+    puts @member.wait_time
+  end
+
+  def wait_url
+    position = params[:QueuePosition]
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say "You are caller number #{position}. Someone will be with you shortly."
+      r.Play 'https://www.dropbox.com/s/z97h9xl4eu47v6d/banana.mp3?dl=1'
     end
 
     render text: response.text
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ MyMedLabsCallCenter::Application.routes.draw do
   root to: "home#index"
   resources :connects, only: :index
   post '/voice' => 'connects#voice', as: :voice
+  post '/enqueue' => 'connects#enqueue', as: :enqueue
+  get '/wait_url' => 'connects#wait_url', as: :wait_url
+  get '/queue' => 'connects#queue', as: :queue
+  get '/about_to_connect' => 'connects#about_to_connect', as: :about_to_connect
 end

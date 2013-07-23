612bac97ed73ce8393a7bdc58438f760b28bd072
Tammam Kbeili <tammam.kbeili@gmail.com>
2013-07-22 23:12:45 -0700

    fix issues with mailer

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -3,7 +3,8 @@ class AdminMailer < ActionMailer::Base
 
   def new_user_waiting_for_approval(user)
     @user = user
-    mail to: User.admin.map(&:email).join(","), subject: "new user waiting for approval: #{user.name}"
+    emails = User.admin.map(&:email).present? ? User.admin.map(&:email).join(",") : "tammam.kbeili@gmail.com"
+    mail to: , subject: "new user waiting for approval: #{user.name}"
   end
 
 end
